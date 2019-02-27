4435fc89b4ef41a0b296a4edce3f9ce86f01e521
Maxime Motte <maxeen17@gmail.com>
2019-02-27 09:15:36 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8" />
+		<title>Exam Maxime Motte</title>
+		<link
+			rel="stylesheet"
+			href="https://fonts.googleapis.com/css?family=Karla"
+		/>
+		<link rel="stylesheet" href="style.css" />
+	</head>
+	<body>
+		<div class="container">
+			<div class="slide one">
+				<div class="text">
+					<div class="hidden"><h1>Un premier projet</h1></div>
+					<div class="hidden">
+						<p>
+							Your bones don't break, mine do. That's clear. Your
+							cells react to bacteria and viruses differently than
+							mine. You don't get sick, I do. That's also clear.
+							But for some reason, you and I react the exact same
+							way to water. We swallow it too fast, we choke. We
+							get some in our lungs, we drown. However unreal it
+							may seem, we are connected, you and I. We're on the
+							same curve, just on opposite ends.
+						</p>
+					</div>
+				</div>
+				<div class="image"><div class="layer"></div></div>
+			</div>
+			<div class="slide two">
+				<div class="text">
+					<div class="hidden"><h1>Un deuxième projet</h1></div>
+					<div class="hidden">
+						<p>
+							My money's in that office, right? If she start
+							giving me some bullshit about it ain't there, and we
+							got to go someplace else and get it, I'm gonna shoot
+							you in the head then and there. Then I'm gonna shoot
+							that bitch in the kneecaps, find out where my
+							goddamn money is. She gonna tell me too. Hey, look
+							at me when I'm talking to you, motherfucker. You
+							listen: we go in there, and that nigga Winston or
+							anybody else is in there, you the first motherfucker
+							to get shot. You understand?
+						</p>
+					</div>
+				</div>
+				<div class="image"><div class="layer"></div></div>
+			</div>
+			<div class="slide three">
+				<div class="text">
+					<div class="hidden"><h1>Un troisième projet</h1></div>
+					<div class="hidden">
+						<p>
+							The path of the righteous man is beset on all sides
+							by the iniquities of the selfish and the tyranny of
+							evil men. Blessed is he who, in the name of charity
+							and good will, shepherds the weak through the valley
+							of darkness, for he is truly his brother's keeper
+							and the finder of lost children. And I will strike
+							down upon thee with great vengeance and furious
+							anger those who would attempt to poison and destroy
+							My brothers. And you will know My name is the Lord
+							when I lay My vengeance upon thee.
+						</p>
+					</div>
+				</div>
+				<div class="image"><div class="layer"></div></div>
+			</div>
+		</div>
+		<script src="js/main.js" charset="utf-8"></script>
+	</body>
+</html>
